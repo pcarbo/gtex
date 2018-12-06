d18af4aa27036386df511f2f34dceed67c09fdfd
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-12-06 10:21:55 -0600

    Created function for plotting PCs 1 & 2.

--- analysis/gtexpca.R
+++ analysis/gtexpca.R
@@ -15,6 +15,9 @@ gtex.data.file <-
   file.path("..","data",
             "GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_median_tpm.gct.gz")
 
+# The PCA plot will be saved to this file.
+pc.plot.file <- "../output/gtexpca.png"
+
 # SET UP ENVIRONMENT
 # ------------------
 # Load the ggplot2 and cowplot libraries, as well as some additional
@@ -25,31 +28,24 @@ source("../code/gtexpca.functions.R")
 
 # LOAD & PREPARE DATA
 # -------------------
-cat("Reading GTEx gene expression data.\n")
 gtex <- read.gtex.data(gtex.data.file)
 cat(sprintf("Total number of tissue types: %d\n",nrow(gtex)))
 cat(sprintf("Total number of genes: %d\n",ncol(gtex)))
 
 # COMPUTE PRINCIPAL COMPONENTS
 # ----------------------------
-cat("Computing PCs from the gene expression data.\n")
 gtex.pca <- prcomp(gtex)
 
-# SUMMARIZE THE TOP 2 PCs
-# -----------------------
-cat("Summary of the first two PCs:\n")
+# SUMMARIZE TOP 2 PCs
+# -------------------
 print(summary(gtex.pca)$importance[,1:2])
-
-stop()
 
 # PLOT TOP 2 PCs
 # --------------
-pdat <- data.frame(tissue = colnames(gtex),
-                   PC1 = gtex.pca$x[,1],
-                   PC2 = gtex.pca$x[,2])
-p <- ggplot(pdat,aes(x = PC1,y = PC2,label = tissue)) +
-       geom_text(color = "gray") +
-       geom_point(shape = 20)
+# Show the projection of the tissues onto PCs 1 and 2, highlighting
+# the brain tissues in magenta.
+p <- plot.gtex.top2pcs(gtex,gtex.pca)
+ggsave(pc.plot.file,p,height = 4,width = 4,dpi = 200)
 
 # SESSION INFO
 # ------------
--- code/gtexpca.functions.R
+++ code/gtexpca.functions.R
@@ -22,3 +22,26 @@ read.gtex.data <- function (file) {
   # the number of genes.
   return(t(out))
 }
+
+# This function creates a plot (a ggplot object) showing the
+# projection of the tissues onto PCs 1 and 2. Brain tissues are
+# highlighted in magenta.
+plot.gtex.top2pcs <- function (gtex, gtex.pca) {
+
+  # For plotting, create a new data frame with four columns: (1)
+  # tissue, (2) whether or not the tissue is a brain tissue; (3) the
+  # first PC, and (4) the second PC.
+  tissues <- rownames(gtex)
+  pdat <- data.frame(tissue = paste(" ",tissues),
+                     brain  = grepl("brain",tissues,ignore.case = TRUE),
+                     PC1    = gtex.pca$x[,"PC1"],
+                     PC2    = gtex.pca$x[,"PC2"])
+
+  # Create the scatterplot, with each point labeled by the tissue
+  # name.
+  return(ggplot(pdat,aes(x = PC1,y = PC2,label = tissue,color = brain)) +
+         geom_text(color = "dodgerblue",size = 3,hjust = 0) +
+         geom_point(shape = 20,size = 2,show.legend = FALSE) +
+         scale_color_manual(values = c("black","magenta")) +
+         theme_cowplot(font_size = 12))
+}
